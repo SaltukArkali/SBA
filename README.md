5f42946b9c8139e96cca5e8eb746849275c04a5b
Saltuk <saltukarkali@gmail.com>
2021-02-20 01:27:01 +0100

    local SBA commit

---
+++ README.md
@@ -0,0 +1,4 @@
+"# SBA" 
+This Project is a base cucumber project of a bank application.
+
+![Parabank Logo](C:\Users\saltu\Documents\SBA\images\ParaBank.jpg)
---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,16 @@
+package stepdefinitions;
+
+public class Hooks {
+
+
+//    @After
+//    public void tearDown(Scenario scenario) {
+//        final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//        if (scenario.isFailed()) {
+//            scenario.embed(screenshot, "image/png");
+//        }
+//        Driver.closeDriver();
+//    }
+
+
+}
